811a2dba43c9b074c73f16712c9a85245fc16e37
Connor Cowan <ccowan003@comcast.net>
2019-07-08 21:42:47 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,6 +1,8 @@
 class Cat
 
+  attr_accessor :mood, :owner
   attr_reader :name           #Cat only reads name, can't change its name
+
   def initialize(name)
     @name = name              #Cat initializes a cat at creation
 
