91bf6284e97d288f1415ed81f1f22430027df6a8
Connor Cowan <ccowan003@comcast.net>
2019-07-08 21:55:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,5 +43,33 @@ class Owner
     Dog.all.select {|dog| dog.owner == self}
   end
 
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
 
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def list_pets
+    number_of_dogs = self.dogs.count
+    number_of_cats = self.cats.count
+    return "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
+  end
+
+  def sell_pets
+    all_pets = self.cats + self.dogs
+    all_pets.each do |pet|
+        sell_pet(pet)
+    end
+  end
+
+  def sell_pet(pet)
+    pet.owner = nil
+    pet.mood = "nervous"
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
 end
