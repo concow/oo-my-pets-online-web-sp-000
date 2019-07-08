5b3244c5883cde637a5cdb68558072b061acb10e
Connor Cowan <ccowan003@comcast.net>
2019-07-08 21:40:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,13 @@
 class Owner
 
   attr_accessor :pets
-  attr_reader :specier, :name     #reader used here because objects can't change species/name
+  attr_reader :species, :name     #reader used here because objects can't change species/name
 
   @@all = []
 
   def initialize(name)
     @name = name
     @species = "human"
-    @@all << self
+    @@all << self               #add all the species objects
   end
 end
