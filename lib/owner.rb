ca83f6dcd325e1e9d5932b018610ea4b9d0d8626
Connor Cowan <ccowan003@comcast.net>
2019-07-08 21:42:01 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -10,4 +10,23 @@ class Owner
     @species = "human"
     @@all << self               #add all the species objects (owners) to our class var array
   end
+
+  def self.all
+    return @@all
+  end
+
+  def self.count
+    return @@all.size
+  end
+
+  def self.reset_all
+    return @@all.clear                  #clear (reset) our owner array
+  end
+
+  def say_species
+     "I am a #{self.species}."
+  end
+
+
+
 end
