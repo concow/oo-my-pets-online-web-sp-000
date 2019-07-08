4728f48bf48a5220c635df81c868c814463e89ee
Connor Cowan <ccowan003@comcast.net>
2019-07-08 21:38:03 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,8 @@
 class Cat
-  
+
   attr_reader :name           #Cat only reads name, can't change its name
   def initialize(name)
     @name = name              #Cat initializes a cat at creation
 
   end
-
 end
