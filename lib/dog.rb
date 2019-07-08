495dcb2aad88a7813d5b48ebc4c8a9f400e3a260
Connor Cowan <ccowan003@comcast.net>
2019-07-08 21:46:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 class Dog
+
   attr_accessor :mood, :owner
   attr_reader :name           #//getter - Dog reads name, cannot change name
-  def initialize(name, owner, mood)
+  @@sll = []
+
+  def initialize(name, owner, mood = "nervous")
     @name = name              #Dog initializes a dog at creation
     @owner = owner
     @mood = mood
+    @@all << sel;f
   end
 
 end
