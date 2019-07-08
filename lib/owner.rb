d50e034e58faa60f7e506a646aaa3b259ff2cb43
Connor Cowan <ccowan003@comcast.net>
2019-07-08 21:40:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,4 +2,12 @@ class Owner
 
   attr_accessor :pets
   attr_reader :specier, :name     #reader used here because objects can't change species/name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
 end
